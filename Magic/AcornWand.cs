32d860eafdee0c2d93430f150407f3eae9b10500
SenkoChansey12543 <142628253+SenkoChansey12543@users.noreply.github.com>
2023-08-30 21:05:05 -0300

    Reworked acorn wand

--- Magic/AcornWand.cs
+++ Magic/AcornWand.cs
@@ -1,8 +1,9 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using TJTTMN.Content.Projectiles;
+using Microsoft.Xna.Framework;
 
 namespace TJTTMN.Content.Items.Weapons.Magic
 {
@@ -10,46 +11,35 @@ namespace TJTTMN.Content.Items.Weapons.Magic
     {
         public override void SetStaticDefaults()
         {
-            // Tooltip.SetDefault("It's better than nothing");
-            // DisplayName.SetDefault("Acorn wand");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
         public override void SetDefaults()
         {
-            Item.damage = 9;
-            Item.mana = 3;
+            Item.damage = 15;
+            Item.mana = 4;
             Item.DamageType = DamageClass.Magic;
             Item.knockBack = 2;
             Item.shootSpeed = 9;
             Item.noMelee = true;
             Item.shoot = ModContent.ProjectileType<AcornShoot>();
             Item.rare = ItemRarityID.White;
-            Item.useTime = 50;
-            Item.useAnimation = 50;
+            Item.useTime = 35;
+            Item.useAnimation = 35;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.width = 25;
             Item.height = 25;
-            Item.autoReuse = false;
-            Item.value = 1000;
+            Item.autoReuse = true;
+            Item.value = Item.buyPrice(silver: 11);
             Item.UseSound = SoundID.Item8;
         }
-        public override void AddRecipes()
+
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
-            Recipe VaritaDeBellotas = CreateRecipe();
-            VaritaDeBellotas.AddIngredient(ItemID.Wood, 15);
-            VaritaDeBellotas.AddIngredient(ItemID.Acorn, 10);
-            VaritaDeBellotas.AddTile(TileID.WorkBenches);
-            VaritaDeBellotas.Register();
-
-
-
-
-
-
-
-
-
-
+            Vector2 muzzleOffset = Vector2.Normalize(velocity) * 30f;
+            if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+            {
+                position += muzzleOffset;
+            }
         }
     }
 }
