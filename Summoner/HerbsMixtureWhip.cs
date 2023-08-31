f48752565d392a9cc7962f2342ed17d48acaeb9c
SenkoChansey12543 <142628253+SenkoChansey12543@users.noreply.github.com>
2023-08-30 21:03:27 -0300

    Added new whip weapon

---
+++ Summoner/HerbsMixtureWhip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.GameContent.Creative;
+using TJTTMN.Content.Buffs.WhipBuffs;
+using TJTTMN.Content.Projectiles.Whips;
+using TJTTMN.Content.Items.Materials;
+using TJTTMN.Content.Tiles;
+
+
+namespace TJTTMN.Content.Items.Weapons.Summoner
+{
+    public class HerbsMixtureWhip : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(HerbsMixtureWhipBuff.TagDamage);
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToWhip(ModContent.ProjectileType<HerbsMixtureWhipProjectile>(), 11, 2, 4, 45);
+            Item.rare = ItemRarityID.White;
+            Item.value = Item.buyPrice(silver: 16, copper: 43);
+        }
+
+        public override bool MeleePrefix()
+        {
+            return true;
+        }
+    }
+}
